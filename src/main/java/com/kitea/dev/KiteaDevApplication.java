de2c0c7e84989ff28bcbf71a46cf1cd7aed59824
yahya66 <yahya.azzouz@etu.uae.ac.ma>
2025-02-10 20:07:28 +0100

    First Push for backend code

---
+++ src/main/java/com/kitea/dev/KiteaDevApplication.java
@@ -0,0 +1,13 @@
+package com.kitea.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiteaDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiteaDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitea/dev/KiteaDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitea.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiteaDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
